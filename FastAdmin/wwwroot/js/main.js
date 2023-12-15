06eb6f276c7aac7cdefd4b74c0dda0d92f93b969
AJBoy\AJ Boy <jebaleali@gmail.com>
2023-12-15 22:39:17 +0330

    Add product UI Update

--- FastAdmin/wwwroot/js/main.js
+++ FastAdmin/wwwroot/js/main.js
@@ -184,7 +184,7 @@
         editimage_cors_hosts: ['picsum.photos'],
         menubar: 'file edit view insert format tools table help',
         toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
-        toolbar_sticky: true,
+        toolbar_sticky: false,
         toolbar_sticky_offset: isSmallScreen ? 102 : 108,
         autosave_ask_before_unload: true,
         autosave_interval: '30s',
